3c5d88b1abaf4844f2353aac4d598f6178322766
MatyBene <benedettimaty@gmail.com>
2024-05-25 21:10:18 -0300

    crea carpeta Files para alojar los archivos de la base de datos. Modifica struct de libros. Carga al archivo binario el mockup de libros

--- books/book.h
+++ books/book.h
@@ -1,6 +1,8 @@
 #ifndef BOOK_H_INCLUDED
 #define BOOK_H_INCLUDED
 
+#define F_BOOKS "Files/F_BOOKS.dat"
+
 typedef struct{
     int bookId; /// único, autoincremental
     char title[100];
@@ -8,6 +10,7 @@ typedef struct{
     char author[50];
     char category[50];
     float rating;
+    int comments;
     int deleted; /// 0 si está activo - 1 si está eliminado
 }stBook;
 
